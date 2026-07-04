c0cae6a0ecb88521d3473c3c4651b8916c07686d
Vivek <vivekaajara337@gmail.com>
2026-07-04 13:51:20 +0530

    Add files via upload

---
+++ Habitrue/Habitrue/HabitrueApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitrueApp.swift
+//  Habitrue
+//
+//  Created by admin on 04/07/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitrueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
